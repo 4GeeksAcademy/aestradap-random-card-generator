f684f0fa6c929055905ce0c138e362c990c1c831
aestradap <55397157+aestradap@users.noreply.github.com>
2024-01-29 17:09:55 +0000

    adding automaticRefresh() funtional-BTN

--- src/app.js
+++ src/app.js
@@ -5,20 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const suite = [
+  '<i style="color:red;" class="bi bi-heart-fill fa-4x"></i>',
+  '<i style="color:red;" class="bi bi-suit-diamond-fill fa-4x"></i>',
+  '<i style="color:black;" class="bi bi-suit-spade-fill fa-4x"></i>',
+  '<i style="color:black;" class="bi bi-suit-club-fill fa-4x"></i>'
+];
 
-  const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-  const suite = [
-    '<i style="color:red;" class="bi bi-heart-fill fa-4x"></i>',
-    '<i style="color:red;" class="bi bi-suit-diamond-fill fa-4x"></i>',
-    '<i style="color:black;" class="bi bi-suit-spade-fill fa-4x"></i>',
-    '<i style="color:black;" class="bi bi-suit-club-fill fa-4x"></i>'
-  ];
+let randomInt = max => Math.floor(Math.random() * max);
 
-  const randomInt = max => Math.floor(Math.random() * max);
-
+const generateCard = () => {
   let cardValueElement = document.getElementById("cardValue");
   let cardSuitOneElement = document.getElementById("suitOne");
   let cardSuitTwoElement = document.getElementById("suitTwo");
@@ -29,4 +26,26 @@ window.onload = function() {
   cardValueElement.innerHTML = value[randomCardValue];
   cardSuitOneElement.innerHTML = suite[randomCardSuite];
   cardSuitTwoElement.innerHTML = suite[randomCardSuite];
+};
+
+window.onload = function() {
+  //write your code here
+  console.log("Hello Rigo from the console!");
+
+  generateCard();
+
+  let refreshing;
+  const element = document.getElementById("autoBtn");
+  element.addEventListener("click", automaticRefresh);
+
+  function automaticRefresh() {
+    if (!refreshing) {
+      refreshing = setInterval(function() {
+        generateCard();
+      }, 5000);
+    } else {
+      clearInterval(refreshing);
+      refreshing = null;
+    }
+  }
 };
